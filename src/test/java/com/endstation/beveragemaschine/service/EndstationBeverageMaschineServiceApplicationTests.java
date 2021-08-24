1241e1541e96b47dd332923ef76599f7228a5840
Lars P <l.pickelin@gmail.com>
2021-08-24 11:44:55 +0200

    Add mysql dependencies + add example

--- src/test/java/com/endstation/beveragemaschine/service/EndstationBeverageMaschineServiceApplicationTests.java
+++ src/test/java/com/endstation/beveragemaschine/service/EndstationBeverageMaschineServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package com.endstation.beveragemaschine.service;
 
+import com.endstation.beveragemaschine.service.configuration.H2TestProfileJPAConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = {
+        EndstationBeverageMaschineServiceApplication.class,
+        H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
 class EndstationBeverageMaschineServiceApplicationTests {
 
     @Test
