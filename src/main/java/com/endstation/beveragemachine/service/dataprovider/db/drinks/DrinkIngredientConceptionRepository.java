3f621a3c35713da38d963067c958588de0bb9932
Lars P <l.pickelin@gmail.com>
2021-09-11 12:23:44 +0200

    Unused dependency

--- src/main/java/com/endstation/beveragemachine/service/dataprovider/db/drinks/DrinkIngredientConceptionRepository.java
+++ src/main/java/com/endstation/beveragemachine/service/dataprovider/db/drinks/DrinkIngredientConceptionRepository.java
@@ -3,8 +3,6 @@ package com.endstation.beveragemachine.service.dataprovider.db.drinks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface DrinkIngredientConceptionRepository extends JpaRepository<DrinkIngredientConceptionEntity, Long> {
 }
